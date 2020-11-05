12b1eebcdb1a205aabbee12ed71e71a71bfc80ea
WindSoilder <WindSoilder@outlook.com>
2020-11-05 13:52:31 +0800

    make use of async std runtime

--- src/bin/mongo_sync.rs
+++ src/bin/mongo_sync.rs
@@ -1,6 +1,9 @@
 use clap::Clap;
 use mongo_sync::SyncerConfig;
+use mongodb::Client;
 use std::fs;
+
+use mongodb::bson::doc;
 
 #[derive(Clap, Debug)]
 #[clap(version = env!("CARGO_PKG_VERSION"), author = env!("CARGO_PKG_AUTHORS"))]
@@ -10,8 +13,13 @@ struct Opts {
     conf: String,
 }
 
-fn main() {
+#[async_std::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opts: Opts = Opts::parse();
+    let client = Client::with_uri_str("mongodb://localhost").await?;
+    let coll = client.database("aa").collection("bb");
 
+    coll.insert_one(doc! {"x": 1}, None).await.unwrap();
     println!("Config file path: {:?}", opts);
+    Ok(())
 }
